74da5e2122fc8f0b9e23ecca2cfe404f2a874d0b
Maher Odeh <maherod@gmail.com>
2016-10-31 03:35:20 -0400

    added lottery.py

---
+++ lottery.py
@@ -0,0 +1,13 @@
+import random
+
+print "simple lottery random numbers generator"
+
+while True:
+    print ""
+    print "Numbers:", random.sample(xrange(1,37), 6)
+    print "Strong Number:", random.sample(xrange(1,7), 1)
+    print ""
+    raw_input("Hit <Enter> for more numbers, <CTRL-C> to Exit>")
+
+
+
