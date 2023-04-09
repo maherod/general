4f97b3c3a52cd6e82c7d484ed0f0f14b6709bc8a
Maher Odeh <maherod@gmail.com>
2023-04-09 13:02:45 +0300

    based the generation on most frequent numbers

--- misc/lottery.py
+++ misc/lottery.py
@@ -1,15 +1,19 @@
-#!/usr/bin/env python
-
 import random
 
-print "(simple lottery random numbers generator"
+# Define a list of the most frequent numbers in the Israeli lottery
+most_frequent_numbers = [5, 7, 14, 19, 21, 26, 30, 32, 36]
 
-while True:
-    print ""
-    print "Numbers:", random.sample(xrange(1,37), 6)
-    print "Strong Number:", random.sample(xrange(1,7), 1)
-    print ""
-    raw_input("Hit <Enter> for more numbers, <CTRL-C> to Exit>")
+# Generate 6 random numbers from 1 to 37
+numbers = random.sample(range(1, 38), 6)
 
+# Choose a "strong" number between 1 to 7
+strong_number = random.randint(1, 7)
 
+# Check if the "strong" number is also one of the most frequent numbers
+# If so, choose another random number until a unique number is found
+while strong_number in most_frequent_numbers:
+    strong_number = random.randint(1, 7)
 
+# Print the results
+print("Numbers: ", sorted(numbers))
+print("Strong number: ", strong_number)
