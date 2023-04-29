e65fc76820dbbe54570ac05e1db771bf634108f2
Maher Odeh <maherod@gmail.com>
2023-04-29 22:05:01 +0300

    fixed comments

--- networking/bandwidth_limiter.sh
+++ networking/bandwidth_limiter.sh
@@ -1,52 +1,53 @@
 #!/bin/bash
 
+# Variables
 DEV="bond0"
-_TC="/sbin/tc"
+TC="/sbin/tc"
 BW="4000mbit"
-U32="$_TC filter add dev $DEV protocol ip parent 1:0 prio 1 u32"
+U32="$TC filter add dev $DEV protocol ip parent 1:0 prio 1 u32"
 DPORT="443"
 PID="/var/run/limiter.pid"
 
-function setl {
-  if [ -f $PID ] ; then
-   echo "Bandwidth limiter already running with $BW set on port $DPORT"
- else
-  $_TC qdisc add dev $DEV root handle 1: htb default 30
-  $_TC class add dev $DEV parent 1: classid 1:1 htb rate $BW
-  $U32 match ip dport $DPORT 0xffff flowid 1:1
-  touch $PID
-fi
-}
-
-function nol {
-  $_TC qdisc del dev $DEV root
-  rm $PID
-}
-
-function showl {
-  if [ -f $PID ] ; then
-    $_TC -s -d class show dev $DEV
+# Functions
+setl() {
+  if [ -f "$PID" ]; then
+    echo "Bandwidth limiter already running with $BW set on port $DPORT"
   else
-    echo "bandwidth limiter not running"
- fi
+    $TC qdisc add dev "$DEV" root handle 1: htb default 30
+    $TC class add dev "$DEV" parent 1: classid 1:1 htb rate "$BW"
+    $U32 match ip dport "$DPORT" 0xffff flowid 1:1
+    touch "$PID"
+  fi
 }
 
+nol() {
+  $TC qdisc del dev "$DEV" root
+  rm "$PID"
+}
+
+showl() {
+  if [ -f "$PID" ]; then
+    $TC -s -d class show dev "$DEV"
+  else
+    echo "Bandwidth limiter not running"
+  fi
+}
+
+# Main
 case "$1" in
   start)
-  setl
-  echo "Bandwidth Limiter is set to BW: $BW on DPORT $DPORT"
-  ;;
-
+    setl
+    echo "Bandwidth limiter is set to BW: $BW on DPORT $DPORT"
+    ;;
   stop)
-  nol
-        echo "Stopping Bandwidth limiter"
-  ;;
-
+    nol
+    echo "Stopping bandwidth limiter"
+    ;;
   status)
-  showl 
-  ;;
-
+    showl
+    ;;
   *)
-    echo $"Usage: $0 {start|stop|status}"
+    echo "Usage: $0 {start|stop|status}"
     exit 1
 esac
+
