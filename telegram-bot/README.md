f7a4f827f8d913a8379f446723a53e58e5bdadb8
Maher Odeh <maherod@gmail.com>
2023-04-29 22:01:20 +0300

    added telegram bot

---
+++ telegram-bot/README.md
@@ -0,0 +1,33 @@
+# Telegram Bot
+
+This is a simple Telegram bot that can execute shell commands and send the output back to the user.
+
+## Prerequisites
+
+- Python 3.9
+- pip
+
+## Requirements
+- telebot==0.0.4-
+- requests==2.26.0
+- pyTelegramBotAPI
+
+
+## Installation
+
+1. Clone this repository: `git clone https://github.com/<your_username>/<your_repo>.git`
+2. Install the required packages: `pip install -r requirements.txt`
+3. Set the `BOT_TOKEN` environment variable to your Telegram bot token: `export BOT_TOKEN=<your_bot_token>`
+
+## Usage
+
+To start the bot, run the following command: `python bot.py`
+
+Once the bot is running, you can interact with it in a Telegram chat. Send the following command to the bot to execute a shell command: `/cmd <command>`
+
+Replace `<command>` with the shell command you want to execute. The bot will execute the command and send the output back to you.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
---
+++ telegram-bot/bot.py
@@ -0,0 +1,41 @@
+#!/bin/env python3
+import telebot
+import os
+import logging
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+bot = telebot.TeleBot(BOT_TOKEN)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch = logging.StreamHandler()
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(commands=['info'])
+def send_info(message):
+    bot.reply_to(message, "I am a bot, my master is Maher Odeh")
+
+@bot.message_handler(commands=['cmd'])
+def execute_command(message):
+    chat_id = message.chat.id
+    command = message.text.split()[1:]
+    command_str = ' '.join(command)
+    try:
+        output = os.popen(command_str).read()
+        bot.send_message(chat_id, output)
+    except OSError as e:
+        logger.error(f"Failed to execute command '{command_str}': {e}")
+        bot.send_message(chat_id, "Failed to execute command")
+    except Exception as e:
+        logger.error(f"An error occurred while executing command '{command_str}': {e}")
+        bot.send_message(chat_id, "An error occurred while executing command")
+
+if __name__ == '__main__':
+    logger.info("Bot started")
+    bot.polling(none_stop=True)
